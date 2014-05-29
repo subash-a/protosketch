a2a4bc19145a3f05985321e04e54ad666ba178cf
subash-a <s7subash@gmail.com>
2014-05-29 23:09:48 +0530

    Added custom stylesheet for better styling Added html write capability, need to test the capability since its is erroneous

--- build_tester.py
+++ build_tester.py
@@ -3,8 +3,10 @@ import page_builder as pb
 
 c = pb.createDocument()
 c = pb.addComponent(c,"input",["0","1","2","3","4","5"])
-jsfiles = ["app.js","yui.js","bootstrap.js"]
-tags = pb.buildScriptTags(jsfiles)
-for x in tags.iter():
-    XML.dump(x)
+jsfiles = ["utils/js/jquery-1.7.1.js","utils/js/bootstrap.js"]
+cssfiles = ["utils/css/bootstrap.css","utils/css/prototype.css"]
+h = pb.createHTMLPage(c,jsfiles,cssfiles)
+et = XML.ElementTree()
+et.write(XML.tostring(h),"single.html")
+
 
--- page_builder.py
+++ page_builder.py
@@ -17,11 +17,17 @@ def createDocument():
     doc.append(comment)
     return doc
 
-def createHTMLPage(xml_template,js_src,css):
+def createHTMLPage(xml_template,js_list,css_list):
     html = XML.Element("html")
     head = XML.SubElement(html,"head")
     body = XML.SubElement(html,"body")
+    css_list = buildStyleTags(css_list)
+    for c in css_list:
+        head.append(c)
     body.append(xml_template)
+    js_list = buildScriptTags(js_list)
+    for j in js_list:
+        body.append(j)
     return html
 
 def buildScriptTags(js_list):
@@ -29,6 +35,16 @@ def buildScriptTags(js_list):
     for x in js_list:
         script = XML.Element("script")
         script.set("src",x)
+        script.text = " "
         tags.append(script)
     return tags
         
+def buildStyleTags(css_list):
+    tags = XML.Element("tags")
+    for x in css_list:
+        link = XML.Element("link")
+        link.set("rel", "stylesheet")
+        link.set("type", "text/css")
+        link.set("src", x)
+        tags.append(link)
+    return tags
