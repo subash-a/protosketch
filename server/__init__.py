5e5043cf9f81951a0382443bd41ec3f29504062b
subash-a <s7subash@gmail.com>
2014-06-18 00:28:44 +0530

    -- Changed paths of a few files for better modularity

---
+++ server/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, url_for, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "hello world!"
+
+@app.route('/index')
+def index_load():
+    return render_template("index.html")
+
+if  __name__ == '__main__':
+    app.debug = True
+    app.run()
+    
